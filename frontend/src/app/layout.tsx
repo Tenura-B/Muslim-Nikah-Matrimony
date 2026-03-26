3b8e9537c92ea2502bd9126304be8a5644c2f099
sajitha00 <sajithageevinda.sd@gmail.com>
2026-03-26 11:13:07 +0530

    up

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono,Poppins,Andada_Pro,Aref_Ruqaa_Ink } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/ui/navbar/Nav";
+import Footer from "@/components/ui/footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,6 +49,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Nav />
         {children}
+        <Footer />
       </body>
     </html>
   );
