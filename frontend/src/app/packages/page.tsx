3ed6cf5682e4a7ee8ba49aac37ae8c6020b7713f
sajitha00 <sajithageevinda.sd@gmail.com>
2026-03-26 17:26:37 +0530

    package start

---
+++ frontend/src/app/packages/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PackagesHeader from '@/components/packages/header'
+
+
+function packages() {
+  return (
+    <div>
+        <PackagesHeader />
+       
+    </div>
+  )
+}
+
+export default packages
